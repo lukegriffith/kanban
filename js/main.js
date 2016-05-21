cd2ab66d23c54d7eae20ffa5d4442167c7db9a17
Luke Griffith <lukemgriffith@gmail.com>
2016-05-22 00:25:57 +0100

    Added render functionality for board object. Amended launch.json to attach and launch. Updated styles

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function postit(header,priority){
 
     post.type = "postit"
 
-    post.name = header,
+    post.header = header,
     post.labels = [],
     post.priority = priority,
     post.addLabel = function (label){
@@ -28,11 +28,102 @@ function swimlane(name,meta) {
     lane.children = []
     
     lane.addChild = function(postit) {
-        children.push(postit);
+        lane.children.push(postit);
     }
     
     return lane
 }
+
+
+
+function renderBoard(board){ 
+    
+    var container;
+    var header;
+    var swimlane;
+    var i;
+    
+    container = document.getElementById("container")
+    container.innerHTML = ""
+    
+    header = document.createElement("h1")
+    
+    //console.log(header)
+    
+    console.log(board);
+    console.log(board.title);
+    header.innerText = board.title;
+    
+    container.appendChild(header);
+    
+    console.log("Starting Swimanes");
+    
+    
+    for(i=0;i<board.swimlanes.length;i++) {
+        console.log("forloop "+i);
+        swimlane = renderSwimlane(board.swimlanes[i])
+        container.appendChild(swimlane)
+    }
+    
+}
+
+function renderSwimlane(obj) { 
+    
+    var div;
+    var header; 
+    var child;
+    var i;
+    
+    div = document.createElement("div");
+    header = document.createElement("h1");
+    
+    header.innerText = obj.name;
+    div.className = "swimlane";
+    
+    div.appendChild(header);
+    
+    //console.log("Starting Postits");
+    
+    //console.log(obj);
+    
+    
+    
+    if (obj.children.length != 0){
+        for(i=0; i < obj.children.length; i++) {
+            
+            //console.log(i);
+            
+            child = renderPostIt(obj.children[i])
+            div.appendChild(child);
+        }
+    }
+    
+    return div;
+    
+}
+
+function renderPostIt(obj) {
+    
+    var div;
+    var header;
+    
+    //console.log("renderPostIT");
+    //console.log(obj);
+    
+    
+    div = document.createElement("div");
+    div.className = "postit";
+    header = document.createElement("h1");
+    header.innerHTML = obj.header;
+    div.appendChild(header);
+    
+    return div
+    
+    
+}
+
+
+
 
 var board =  {
 
@@ -53,75 +144,74 @@ var board =  {
 }
 
 
-
-console.log(board);
-console.log("boardCreated");
-console.log("changing name of board");
-
-board.renameBoard("New Name");
-console.log(board);
-
+// 
+// console.log(board);
+// console.log("boardCreated");
+// console.log("changing name of board");
+// 
+// //  board.renameBoard("New Name");
+// console.log(board);
+// 
 console.log("constructing lane")
 var lane = swimlane("ToDo");
-
-console.log(lane);
-
+// 
+// console.log(lane);
+var post;
+post = postit("child");
+// 
+lane.addChild(post);
+// 
+// 
+// 
 console.log("adding to board");
 board.addSwimlane(lane);
-console.log(board);
+// 
+ lane = swimlane("next");
+ lane.addChild(post);
+ lane.addChild(post);
+ lane.addChild(post);
+// 
+ board.addSwimlane(lane);
+ 
+// 
+// console.log(board);
+// 
 
-
-
-
-// Look at creating an interface to access the board, opposed to accessing the board directly in rendering.
-// Use methods like GetSwimLanes, and GetPostIts opposed to accessing board items directly through enumerations.
-// Abstract it through another function.
-
-function renderItem(item,parent){
-    
-    switch (item.type) { 
-        
-        case "postit":
-            renderPostIt(item, parent);
-            
-        case "swimlane":
-        renderSwimLane(item, parent)
-        
-    }
-    
-}
-
-
-
-var swimlaneCount = 0;
 
 var addSwimlane = document.getElementById("AddSwimlane")
-
 addSwimlane.addEventListener("click", function(e) {
     
-    
-
-
-    console.log("getting swimlane");
-    var swimlane = document.getElementById("hidden");
-    var clone = swimlane.cloneNode(true);
-    
-    swimlaneCount = swimlaneCount + 1
-    clone.id = swimlaneCount 
-    clone.className = "swimlane"
-    
-    var container = document.getElementById("container")
-    
-    
-    container.appendChild(clone);
-    
-    var headerValue = document.getElementById("SwimLaneName").value
-    
-    document.getElementById(swimlaneCount).innerHTML = headerValue
-    
-    
-    
-    
+    var header = document.getElementById("SwimLaneName").value; 
+    var swim = swimlane(header);
+    board.addSwimlane(swim);
+    renderBoard(board); 
     
 })
+
+renderBoard(board);
+
+
+//    // Code was used to add a swimlane to the board. 
+//    console.log("getting swimlane");
+//    var swimlane = document.getElementById("hidden");
+//    var clone = swimlane.cloneNode(true);
+//    
+//    swimlaneCount = swimlaneCount + 1
+//    clone.id = swimlaneCount 
+//    clone.className = "swimlane"
+//    
+//    var container = document.getElementById("container")
+//    
+//    
+//    container.appendChild(clone);
+//    
+//    var headerValue = document.getElementById("SwimLaneName").value
+//    
+//    document.getElementById(swimlaneCount).innerHTML = headerValue
+    
+    
+    
+    
+    
+
  
